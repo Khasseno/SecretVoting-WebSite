e0d8ee95294c2751a653f7aa91f4367df348ae86
Алтай Хасенов <107077147+Khasseno@users.noreply.github.com>
2023-06-04 20:18:58 +0600

    Update connect.php

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,6 +1,11 @@
 <?php
 
-    $connect = mysqli_connect('localhost', 'root', 'root', 'test');
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'test';
+
+    $connect = mysqli_connect($hostname, $username, $password, $dbname);
 
     if (!$connect)
     {
